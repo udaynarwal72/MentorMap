b887c89dc32d54d309d1064eeb2a0b6e7c16af59
raghzzzspace <162113357+raghzzzspace@users.noreply.github.com>
2024-08-30 18:02:18 +0530

    added chatbot styling

--- apps/Website-Backend/src/routes/index.js
+++ apps/Website-Backend/src/routes/index.js
@@ -10,7 +10,7 @@ router.post('/chat', async (req, res) => {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const prompt = "Help me with this statement:" + req.body.message;
+    const prompt = "Help me with this statement:Give answer in a decent english language" + req.body.message;
 
     const result = await model.generateContent(prompt);
     console.log(result.response.text());
